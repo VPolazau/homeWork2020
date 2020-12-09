9e27d4d137dd6c8e7a689bfdc737c51fa76b8f82
Victor <VPolazau@gmail.com>
2020-12-09 23:04:45 +0300

    Introducing arrow functions

---
+++ 17/arrow_fn.js
@@ -0,0 +1,44 @@
+(function () {
+  const mas = [5, 2, 6, 9, 2, 1, 0, 4, 7, 8, 10, 3];
+  mas.sort((a, b) => a - b);
+  // console.log(mas.filter((a) => a % 2));
+  // console.log(mas.map(n => ({digit: n})));
+
+  // 1.Переделать функцию с использованием функции-стрелки (в методе reduce тоже использовать arrow function):
+  //
+  //   function sum() {
+  //     const params = Array.prototype.slice.call(arguments);
+  //
+  //     if (!params.length) return 0;
+  //
+  //     return params.reduce(function (prev, next) { return prev + next; });
+  //   }
+  //
+  //   sum(1, 2, 3, 4); // 10
+  //   sum(); // 0
+
+  //
+  // function sum() {
+  //   const params = Array.prototype.slice.call(arguments);
+  //
+  //   if (!params.length) return 0;
+  //
+  //   return params.reduce(function (prev, next) { return prev + next; });
+  // }
+
+  
+
+  const sum = (...masNum) => { return !masNum.length ? 0 : masNum.reduce((prev, next) => next + prev) } // короткий способ записи
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+  //Более читабельный вариант
+  //   if (!masNum.length) {
+  //     return 0;
+  //   }
+  //   return masNum.reduce((prev, next) => next + prev)
+  // }
+
+  console.log(sum(1, 2, 3, 4)); // 10
+  console.log(sum()); // 0
+})();
