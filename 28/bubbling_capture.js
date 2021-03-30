4fdc541eaf6f69fd184dc2f4ffcf60ea9bce0b4c
Victor <VPolazau@gmail.com>
2021-03-30 14:59:03 +0300

    ToDo first commit

---
+++ 28/bubbling_capture.js
@@ -0,0 +1,34 @@
+let html = document.documentElement;
+let body = document.body;
+let div = body.querySelector("div");
+let ul = body.querySelector("ul");
+let li = body.querySelector("li");
+let pause = 200;
+
+// Bubble
+html.addEventListener("click", callback, false);
+body.addEventListener("click", callback, false);
+div.addEventListener("click", callback, false);
+html.addEventListener("click", callback, false);
+ul.addEventListener("click", callback, false);
+li.addEventListener("click", callback, false);
+
+// Capture
+html.addEventListener("click", callback, true);
+body.addEventListener("click", callback, true);
+div.addEventListener("click", callback, true);
+html.addEventListener("click", callback, true);
+ul.addEventListener("click", callback, true);
+li.addEventListener("click", callback, true);
+
+function callback(event) {
+  let ms = (event.timeout = event.timeout + pause || 0);
+  let target = event.currentTarget;
+
+  setTimeout(function () {
+    target.classList.add("highlight");
+    setTimeout(function () {
+      target.classList.remove("highlight");
+    }, pause);
+  }, ms);
+}
---
+++ 29_toDoList/app.js
@@ -0,0 +1,91 @@
+const tasks = [
+  {
+    _id: "5d2ca9e2e03d40b326596aa7",
+    completed: true,
+    body:
+      "Occaecat non ea quis occaecat ad culpa amet deserunt incididunt elit fugiat pariatur. Exercitation commodo culpa in veniam proident laboris in. Excepteur cupidatat eiusmod dolor consectetur exercitation nulla aliqua veniam fugiat irure mollit. Eu dolor dolor excepteur pariatur aute do do ut pariatur consequat reprehenderit deserunt.\r\n",
+    title: "Eu ea incididunt sunt consectetur fugiat non.",
+  },
+  {
+    _id: "5d2ca9e29c8a94095c1288e0",
+    completed: false,
+    body:
+      "Aliquip cupidatat ex adipisicing veniam do tempor. Lorem nulla adipisicing et esse cupidatat qui deserunt in fugiat duis est qui. Est adipisicing ipsum qui cupidatat exercitation. Cupidatat aliqua deserunt id deserunt excepteur nostrud culpa eu voluptate excepteur. Cillum officia proident anim aliquip. Dolore veniam qui reprehenderit voluptate non id anim.\r\n",
+    title:
+      "Deserunt laborum id consectetur pariatur veniam occaecat occaecat tempor voluptate pariatur nulla reprehenderit ipsum.",
+  },
+  {
+    _id: "5d2ca9e2e03d40b3232496aa7",
+    completed: true,
+    body:
+      "Occaecat non ea quis occaecat ad culpa amet deserunt incididunt elit fugiat pariatur. Exercitation commodo culpa in veniam proident laboris in. Excepteur cupidatat eiusmod dolor consectetur exercitation nulla aliqua veniam fugiat irure mollit. Eu dolor dolor excepteur pariatur aute do do ut pariatur consequat reprehenderit deserunt.\r\n",
+    title: "Eu ea incididunt sunt consectetur fugiat non.",
+  },
+  {
+    _id: "5d2ca9e29c8a94095564788e0",
+    completed: false,
+    body:
+      "Aliquip cupidatat ex adipisicing veniam do tempor. Lorem nulla adipisicing et esse cupidatat qui deserunt in fugiat duis est qui. Est adipisicing ipsum qui cupidatat exercitation. Cupidatat aliqua deserunt id deserunt excepteur nostrud culpa eu voluptate excepteur. Cillum officia proident anim aliquip. Dolore veniam qui reprehenderit voluptate non id anim.\r\n",
+    title:
+      "Deserunt laborum id consectetur pariatur veniam occaecat occaecat tempor voluptate pariatur nulla reprehenderit ipsum.",
+  },
+];
+
+(function (arrOfTasks) {
+  const objOfTasks = arrOfTasks.reduce((acc, task) => {
+    acc[task._id] = task;
+    return acc;
+  }, {});
+
+  //Elements UI
+  const listContainer = document.querySelector(
+    ".tasks-list-section .list-group"
+  );
+
+  renderAllTasks(objOfTasks);
+
+  function renderAllTasks(tasksList) {
+    if (!tasksList) {
+      console.error("Передайте список задач!");
+      return;
+    }
+
+    const fragment = document.createDocumentFragment();
+
+    // Object.values() -> принимаем объект и возвращаем ввиде массива, соответсвенно можем его теперь перебрать
+    Object.values(tasksList).forEach((task) => {
+      const li = listItemTemplate(task);
+      fragment.appendChild(li);
+    });
+    listContainer.appendChild(fragment);
+  }
+
+  // Наполняю ЛИшку содержимым
+  function listItemTemplate({ _id, title, body } = {}) {
+    const li = document.createElement("li");
+    li.classList.add(
+      "list-group-item",
+      "d-flex", // display для создания гибкого контейнера, трансформирует прямые дочерние элементы в «гибкие»
+      "align-items-center", // относится к d-flex, для выравнивания по высоте
+      "flex-wrap", // обертка или выравнивание содержимого
+      "mt-2" // marginTop-2
+    );
+    const span = document.createElement("span");
+    span.textContent = title;
+    span.style.fontWeight = "bold";
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "Delete task";
+    deleteBtn.classList.add("btn", "btn-danger", "ml-auto", "delete-btn"); //marginLeft
+
+    const article = document.createElement("p");
+    article.textContent = body;
+    article.classList.add("mt-2", "w-100"); // width 100%
+
+    li.appendChild(span);
+    li.appendChild(article);
+    li.appendChild(deleteBtn);
+
+    return li;
+  }
+})(tasks);
