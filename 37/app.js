52bf0c81698dd9cfe6d06c2c57b145c32cdf2152
Victor <VPolazau@gmail.com>
2021-08-20 15:25:19 +0300

    actualize "GET" request

--- 37/app.js
+++ 37/app.js
@@ -1,6 +1,5 @@
 const btnGetUsers = document.querySelector(".btn-get-users");
 const btnAddUser = document.querySelector(".btn-add-user");
-const container = document.querySelector(".container");
 
 function getUsers(cb) {
   const xhr = new XMLHttpRequest();
@@ -19,10 +18,51 @@ function getUsers(cb) {
 }
 
 btnGetUsers.addEventListener("click", (e) => {
-  getUsers((respons) => {
-    // const fragment = document.createDocumentFragment();
+  getUsers((response) => {
+    const accordion = document.querySelector(".accordion");
+    // Чтобы аккордион раcкрывался по одному, а не все сразу, значения атрибутов: accordionBtn(data-bs-target, aria-controls) и accordionCollapse(id) должны отличаться на каждом шаге.  Поэтому добавил переменную 'n'
+    let n = 1;
     response.forEach((user) => {
-      //   const accordion = document.createDocumentFragment();
+      // Создаём елементы с нужными атрибутами для аккордиона(bootstrap)
+      const accordionItem = document.createElement("div");
+      accordionItem.classList.add("accordion-item");
+      accordionItem.classList.add("mt-2");
+
+      const accordionBtn = document.createElement("button");
+      accordionBtn.classList.add("accordion-button");
+      accordionBtn.setAttribute("data-bs-toggle", "collapse");
+      accordionBtn.setAttribute("data-bs-target", "#collapse" + n);
+      accordionBtn.setAttribute("aria-expanded", "true");
+      accordionBtn.setAttribute("aria-controls", "collapse" + n);
+
+      const accordionTitle = document.createElement("div");
+      accordionTitle.classList.add("accordion-title");
+      //  Заполняем title
+      accordionTitle.innerText = user.name;
+
+      const accordionCollapse = document.createElement("div");
+      accordionCollapse.classList.add("accordion-collapse");
+      accordionCollapse.classList.add("collapse");
+      accordionCollapse.setAttribute("id", "collapse" + n);
+      accordionCollapse.setAttribute("aria-labelledby", "headingOne");
+
+      const accordionBody = document.createElement("div");
+      accordionBody.classList.add("accordion-body");
+      // Заполняем body
+      accordionBody.innerText = `Email: ${user.email},
+        @Username: ${user.username},
+        Phone: ${user.phone},
+        Website: ${user.website}.`;
+
+      // Вкладываем друг в друга
+      accordionItem.appendChild(accordionBtn);
+      accordionItem.appendChild(accordionCollapse);
+      accordionBtn.appendChild(accordionTitle);
+      accordionCollapse.appendChild(accordionBody);
+
+      //добавляем в наш fragment accordionItem
+      accordion.appendChild(accordionItem);
+      n++;
     });
   });
 });
