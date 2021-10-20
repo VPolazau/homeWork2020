bca8afc8171ad93151b33fd7747b07a33b9e6a68
Victor <VPolazau@gmail.com>
2021-10-20 16:18:57 +0300

    first task

---
+++ 51_classEs6/app.js
@@ -0,0 +1,32 @@
+// 1.  Реализовать конструктор в ES6 синтаксисе (также используйте аргументы по умолчанию):
+//
+//     function Component(tagName) {
+//       this.tagName = tagName || 'div';
+//       this.node = document.createElement(tagName);
+//     }
+//
+//     Пример вызова:
+//
+//     const comp = new Component('span');
+
+class Component {
+  constructor(tagName) {
+    this.tagName = tagName || "div";
+    this.node = document.createElement(tagName);
+  }
+}
+const comp = new Component("span");
+
+// 2.  Реализовать конструктор и методы в ES6 синтаксисе:
+//
+//     function Component(tagName) {
+//       this.tagName = tagName || 'div';
+//       this.node = document.createElement(tagName);
+//     }
+//
+//     Component.prototype.setText = function (text) {
+//       this.node.textContent = text;
+//     };
+
+// 3.  Создать класс калькулятора который будет принимать стартовое значение и у него будут
+//     методы сложить, вычесть, умножить , разделить. Также у него должны быть геттер и сеттер для получения и установки текущего числа с которым производятся вычисления.
