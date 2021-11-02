bd31016114cf17a44daeff47905a17ebe2db7848
Victor <VPolazau@gmail.com>
2021-11-02 14:23:15 +0300

    header layout

--- 57_AviaTickets/src/js/plugins/materialize.js
+++ 57_AviaTickets/src/js/plugins/materialize.js
@@ -1,6 +1,10 @@
-import "materialize-css/dist/css/materialize.min.css";
-import "materialize-css/dist/js/materialize.min.js";
+import 'materialize-css/dist/css/materialize.min.css';
+import 'materialize-css/dist/js/materialize.min.js';
 
 // init select
-const select = document.querySelectorAll("select");
+const select = document.querySelectorAll('select');
 M.FormSelect.init(select);
+
+export function getSelectInstance(elem) {
+  return M.FormSelect.getInstance(elem);
+}
