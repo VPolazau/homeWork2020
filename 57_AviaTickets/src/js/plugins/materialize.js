4e033f5d01ce77afaa4db38c9c18098f472fbc1c
Victor <VPolazau@gmail.com>
2021-11-02 15:50:32 +0300

    input card layout

--- 57_AviaTickets/src/js/plugins/materialize.js
+++ 57_AviaTickets/src/js/plugins/materialize.js
@@ -8,3 +8,29 @@ M.FormSelect.init(select);
 export function getSelectInstance(elem) {
   return M.FormSelect.getInstance(elem);
 }
+
+// init autocomplete
+const autocomplete = document.querySelectorAll('.autocomplete');
+M.Autocomplete.init(autocomplete, {
+  data: {
+    Apple: null,
+    Microsoft: null,
+    Google: 'https://placehold.it/250x250',
+  },
+});
+
+export function getAutocompleteInstance(elem) {
+  return M.Autocomplete.getInstance(elem);
+}
+
+// init datepickers
+const datepickers = document.querySelectorAll('.datepicker');
+M.Datepicker.init(datepickers, {
+  showClearBtn: true,
+  format: 'dd mmm, yyyy',
+  // autoClose: true,
+});
+
+export function getDatePickerInstance(elem) {
+  return M.Datepicker.getInstance(elem);
+}
