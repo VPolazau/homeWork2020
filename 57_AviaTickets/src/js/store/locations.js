e8bb3c7141f6dcc1c8414a026b42cdfd5696497b
Victor <VPolazau@gmail.com>
2021-11-05 16:32:55 +0300

    add tickets

--- 57_AviaTickets/src/js/store/locations.js
+++ 57_AviaTickets/src/js/store/locations.js
@@ -6,30 +6,56 @@ class Locations {
     this.countries = null;
     this.cities = null;
     this.shortCitiesList = null;
+    this.airlines = null;
+    this.lastSearch = {};
   }
   async init() {
     const response = await Promise.all([
       this.api.countries(),
       this.api.cities(),
+      this.api.airlines(),
     ]);
 
-    const [countries, cities] = response;
+    const [countries, cities, airlines] = response;
     this.countries = this.serializeCountries(countries);
     this.cities = this.serializeCities(cities);
     this.shortCitiesList = this.createShortCitisList(this.cities);
+    this.airlines = this.serializeAirlines(airlines);
 
     return response;
   }
 
   getCityCodeByKey(key) {
-    return this.cities[key].code;
+    const city = Object.values(this.cities).find(
+      item => item.full_name === key
+    );
+    return city.code;
+  }
+
+  getCityNameByCode(code) {
+    return this.cities[code].name;
+  }
+
+  getAirlineNameByCode(code) {
+    return this.airlines[code] ? this.airlines[code].name : '';
+  }
+
+  getAirlineLogoByCode(code) {
+    return this.airlines[code] ? this.airlines[code].logo : '';
   }
 
   createShortCitisList(cities) {
-    // { 'city, country': null }
-    // Object.entries => [key, value]
-    return Object.entries(cities).reduce((acc, [key]) => {
-      acc[key] = null;
+    return Object.entries(cities).reduce((acc, [, city]) => {
+      acc[city.full_name] = null;
+      return acc;
+    }, {});
+  }
+
+  serializeAirlines(airlines) {
+    return airlines.reduce((acc, item) => {
+      item.logo = `https://pics.avs.io/200/200/${item.code}.png`;
+      item.name = item.name || item.name_translations.en;
+      acc[item.code] = item;
       return acc;
     }, {});
   }
@@ -43,25 +69,34 @@ class Locations {
   }
 
   serializeCities(cities) {
-    // { 'City name, Countrie name': city.country_code === code }
     return cities.reduce((acc, city) => {
-      const country_name = this.getCountryNameByCode(city.country_code);
-
-      // Если city.name===null, то оператор 'или' выберет второй
+      const country_name = this.countries[city.country_code].name;
+      const full_name = `${city.name}, ${country_name}`;
       const city_name = city.name || city.name_translations.en;
-      const key = `${city_name}, ${country_name}`;
-      acc[key] = city;
+      acc[city.code] = {
+        ...city,
+        country_name,
+        full_name,
+      };
       return acc;
     }, {});
-  }
-  getCountryNameByCode(code) {
-    // { 'country code': {...}}
-    return this.countries[code].name;
   }
 
   async fetchTickets(params) {
     const response = await this.api.prices(params);
-    console.log(response);
+    this.lastSearch = this.serializeTickets(response.data);
+  }
+
+  serializeTickets(tickets) {
+    return Object.values(tickets).map(tiket => {
+      return {
+        ...tiket,
+        origin_name: this.getCityNameByCode(ticket.origin),
+        destination_name: this.getCityNameByCode(ticket.destination),
+        airline_logo: this.getAirlineLogoByCode(tiket.airline),
+        airline_name: this.getAirlineNameByCode(tiket.airline),
+      };
+    });
   }
 }
 
