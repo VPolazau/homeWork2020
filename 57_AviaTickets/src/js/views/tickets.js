e108bfb292f8fdb1ead90e3554193ee8c1c8c88a
Victor <VPolazau@gmail.com>
2021-11-08 16:30:04 +0300

    tickets.js entry

---
+++ 57_AviaTickets/src/js/views/tickets.js
@@ -0,0 +1,26 @@
+class TicketsUI{
+  constructor(){
+    this.container = document.querySelector('.tickets-sections .row');
+  }
+
+  renderTickets(){
+
+  }
+  clearContainer(){
+
+  }
+  showEmptyMsg(){
+
+  }
+
+  static emptyMsgTemplate(){
+
+  }
+
+  static ticketTemplate(ticket){
+
+  }
+}
+
+const ticketsUI = new TicketsUI();
+import default ticketsUI;
